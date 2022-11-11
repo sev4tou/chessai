9fa12f9d3414529e2d1a01f3cbc119a6f03e3f3b
sev4tou <79899706+sev4tou@users.noreply.github.com>
2022-11-11 10:20:18 +0000

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
+#include<array>
 #include<string.h>
 #include<gtk/gtk.h>
 #include<sstream>
 #include<sys/stat.h>
+#ifndef G_APPLICATION_DEFAULT_FLAGS 
+#define G_APPLICATION_DEFAULT_FLAGS G_APPLICATION_FLAGS_NONE
+#endif
 
 namespace common {
 	bool fileExisted(const char *file) {
@@ -28,13 +32,13 @@ namespace common {
 };
 
 enum PIECE {
-	BLANK,
-	PAWN,
-	ROOK,
-	KNIGHT,
-	BISHOP,
-	QUEEN,
-	KING
+	BLANK = 0,
+	PAWN = 1,
+	ROOK = 2,
+	KNIGHT = 3, 
+	BISHOP = 4,
+	QUEEN = 5,
+	KING = 6
 };
 
 enum SIDE {
@@ -49,9 +53,181 @@ struct rgba_t {
 	float r;
 };
 
+static unsigned char startDat[32]{18, 0, 0, 169, 19, 0, 0, 185, 20, 0, 0, 201, 21, 0, 0, 217, 22, 0, 0, 233, 20, 0, 0, 201, 19, 0, 0, 185, 18, 0, 0, 169};
+
 struct Board {
-	constexpr static unsigned char startDat[32]{18, 0, 0, 169, 19, 0, 0, 185, 20, 0, 0, 201, 21, 0, 0, 217, 22, 0, 0, 233, 20, 0, 0, 201, 19, 0, 0, 185, 18, 0, 0, 169};
 	unsigned char dat[32];
+	unsigned char flg[4]{};
+
+	static void inline setMove(unsigned char *buf, int id, int x, int y) {
+		buf[id] = (x<<3) | y;
+	}
+
+	inline void move(int x1, int y1, int x2, int y2) {
+		downEnpassant(!getWhiteTurn());
+		switch(getPiece(x1,y1)&0b0111) {
+			case PIECE::PAWN:
+				if(y1-y2==2||y1-y2==-2) upEnpassant(!getWhiteTurn(), x1);
+				if(getPiece(x2,y2)==0&&x2!=x1) setPiece(x2,y1,0);
+				break;
+			case PIECE::KING:
+				upKingMoved(!getWhiteTurn());
+				break;
+			case PIECE::ROOK:
+				if(y1 == ((getPiece(x1, y1)&0b1000)?0:7))
+					if(x1==0) upRookMoved(!getWhiteTurn(), 1);
+					else if(x1==7) upRookMoved(!getWhiteTurn(), 0);
+				break;
+		}
+		setPiece(x2,y2,getPiece(x1,y1)&0b1111);
+		setPiece(x1,y1,0);
+		flg[2]^=(1<<6);
+	}
+	void listMove(const int x, const int y, unsigned char *buf, int preview) {
+		int p = getPiece(x, y);
+		int s = 1 - ((p&0b1000)>>2);
+ 		static const int drx[]={1,-1,0,0};
+		static const int dry[]={0,0,-1,1};
+		 
+		memset(buf, -1, 32);
+		switch (p & 0b111) {
+			case PIECE::BLANK:
+				break;
+			case PIECE::PAWN:
+				if((getPiece(x, y+s)&0b0111) == PIECE::BLANK) {
+					setMove(buf, 0, x, y+s);
+					if(y == (s==1? 1 : 6) && (getPiece(x, y+2*s)&0b0111) == PIECE::BLANK)
+						setMove(buf, 1, x, y+2*s);
+				}
+				if(x<7 && ((getPiece(x+1, y+s)&0b111) != PIECE::BLANK) && ((getPiece(x+1, y+s)&0b1000) ^ (p&0b1000)))
+					setMove(buf, 2, x+1, y+s);
+				if(x>0 && ((getPiece(x-1, y+s)&0b111) != PIECE::BLANK) && ((getPiece(x-1, y+s)&0b1000) ^ (p&0b1000)))
+					setMove(buf, 3, x-1, y+s);
+            if(x<7 && y == (s==1? 4 : 3) && ((getPiece(x+1, y)) == (PIECE::PAWN | ((p&0b1000)^0b1000))) && getEnpassant(!((p&0b1000)>>3), x+1))   
+					setMove(buf, 4, x+1, y+s);
+            if(x>0 && y == (s==1? 4 : 3) && ((getPiece(x-1, y)) == (PIECE::PAWN | ((p&0b1000)^0b1000))) && getEnpassant(!((p&0b1000)>>3), x-1)) 
+					setMove(buf, 5, x-1, y+s);
+				break;
+			case PIECE::KNIGHT:
+				static const int dx[]={2,2,-2,-2,1,1,-1,-1};
+				static const int dy[]={1,-1,1,-1,2,-2,2,-2};
+				for(int i=0;i<8;++i) {
+					int u = x+dx[i], v = y+dy[i];
+					if(u>=0 && u<8 && v>=0 && v<8 && !((getPiece(u,v)&0b111) && (getPiece(u,v)&0b1000)==(p&0b1000)))
+							setMove(buf, i, u, v);
+				}
+				break;
+		   case PIECE::QUEEN:
+				for(int i=0,j=16;i<4;++i) {
+					int u=x, v=y;
+					while(true) {
+						u+=drx[i];
+						v+=dry[i];
+						if(u>=0 && u<8 && v>=0 && v<8 && !((getPiece(u,v)&0b111) && (getPiece(u,v)&0b1000)==(p&0b1000))) {
+							setMove(buf, j++, u, v);
+							if(getPiece(u,v)&0b111) break;
+						}
+						else break;
+					}
+				} 
+				[[fallthrough]]
+			case PIECE::BISHOP:
+				static const int dbx[]={1,1,-1,-1};
+				static const int dby[]={1,-1,1,-1};
+				for(int i=0,j=0;i<4;++i) {
+					int u=x, v=y;
+					while(true) {
+						u+=dbx[i];
+						v+=dby[i];
+						if(u>=0 && u<8 && v>=0 && v<8 && !((getPiece(u,v)&0b111) && (getPiece(u,v)&0b1000)==(p&0b1000))) {
+							setMove(buf, j++, u, v);
+							if(getPiece(u,v)&0b111) break;
+						}
+						else break;
+					}
+				}
+				break;
+			case PIECE::ROOK:
+				for(int i=0,j=0;i<4;++i) {
+					int u=x, v=y;
+					while(true) {
+						u+=drx[i];
+						v+=dry[i];
+						if(u>=0 && u<8 && v>=0 && v<8 && !((getPiece(u,v)&0b111) && (getPiece(u,v)&0b1000)==(p&0b1000))) {
+							setMove(buf, j++, u, v);
+							if(getPiece(u,v)&0b111) break;
+						}
+						else break;
+					}
+				}                            
+				break;
+			case PIECE::KING: {
+				std::array<bool, 64> mark{};
+				if(!preview)
+					for(int i=0;i<8;++i)
+						for(int j=0; j<8;++j)
+							if(((getPiece(i,j)&0b1000)^(p&0b1000)) && (getPiece(i,j)&0b111)) {
+								unsigned char a[32];
+								listMove(i, j, a, 1);
+								for(int i=0;i<32;++i)
+									if(a[i]!=0b11111111)
+										mark[a[i]]=1;
+							}
+			 
+				for(int i=0;i<4;++i) {
+					int u = x+drx[i], v = y+dry[i];
+					if(u>=0 && u<8 && v>=0 && v<8 && !((getPiece(u,v)&0b111) && (getPiece(u,v)&0b1000)==(p&0b1000)) && !mark[(u<<3)|v])
+						setMove(buf, i, u, v);
+				}
+				for(int i=0;i<4;++i) {
+					int u = x+dbx[i], v = y+dby[i];
+					if(u>=0 && u<8 && v>=0 && v<8 && !((getPiece(u,v)&0b111) && (getPiece(u,v)&0b1000)==(p&0b1000)) && !mark[(u<<3)|v])  
+						setMove(buf, i+4, u, v);
+				}
+				if(!getKingMoved(p&0b1000>>3) && !mark[(x<<3)|y] && !preview) {
+					if(!getRookMoved((p&0b1000)>>3, 1)) {
+						if((getPiece(1,y)&0b0111) == PIECE::BLANK) {
+							int i;
+							for(i=2;i<=3;i++)
+								if((getPiece(i,y)&0b0111) != PIECE::BLANK || mark[(i<<3)|y]) break;
+							if(i==4) setMove(buf, 9, 2, y);
+						}
+					}
+					if(!getRookMoved((p&0b1000)>>3, 0)) {
+						int i;
+						for(i=5;i<=6;i++)
+							if((getPiece(i,y)&0b0111) != PIECE::BLANK || mark[(i<<3)|y]) break;
+						if(i==7) setMove(buf, 10, 6, y);
+					}
+				}
+				break;
+			}
+		}
+
+
+		if(!preview)
+			for(int i=0;i<32;++i) {
+				if(buf[i]==0b11111111)continue;
+				Board tmp=*this;
+				tmp.move(x,y,buf[i]/8, buf[i]%8);
+				if(tmp.isCheck(p&0b1000))
+					buf[i]=0b11111111;
+			}
+	}
+
+	bool isCheck(int side) {
+		for(int i=0;i<8;++i)
+			for(int j=0;j<8;++j)
+				if(((getPiece(i,j)&0b1000)^side) && (getPiece(i,j)&0b111)) {
+					unsigned char a[32];
+					listMove(i, j, a, 1);
+					for(int i=0;i<32;++i)
+						if(a[i]!=0b11111111)
+							if(getPiece(a[i]/8,a[i]%8)==((side)|(PIECE::KING)))
+								return true;
+				}
+		return false;
+	}
 
 	void exportBoard(const char *str) {
 		FILE *f = fopen(str, "w");
@@ -86,42 +262,53 @@ struct Board {
 		return(dat[id/2]&mask)>>((y&1)<<2);
 	}
 
-	inline int getEnpassant(int side, int row) {
-		return dat[side*8+row]==0b1000;
+	inline bool getEnpassant(int side, int row) {
+		return flg[side]&(1<<row);
 	}
-	inline int getRookMoved(int side, int islong) {
-		return dat[side*2+islong+16]==0b1000;
+	inline bool getRookMoved(int side, int islong) {
+		return flg[2]&(1<<((side<<1)+islong)); 
 	}
-	inline int getKingMoved(int side) {
-		return dat[side*2+20]==0b1000;
+	inline bool getKingMoved(int side) {
+		return flg[2]&(1<<(side+4));
 	}
+	inline bool getWhiteTurn() {
+		return flg[2]&(1<<6);
+	}
+
 	inline void upEnpassant(int side, int row) {
-		dat[side*8+row]|=0b1000;
+		flg[side]|=(1<<row);
 	}
 	inline void upRookMoved(int side, int islong) {
-		dat[side*2+islong+16]|=0b1000;
+		flg[2]|=(1<<((side<<1)+islong)); 
 	}
 	inline void upKingMoved(int side) {
-		dat[side*2+20]|=0b1000;
+		flg[2]|=(1<<(side+4));
 	}
- 
-	inline void downEnpassant(int side, int row) {
-		dat[side*8+row]&=0b0111;
+	inline void upWhiteTurn() {
+		flg[2]|=(1<<6);
+	}
+
+	inline void downEnpassant(int side) {
+		flg[side]=0;
 	}
 	inline void downRookMoved(int side, int islong) {
-		dat[side*2+islong+16]&=0b0111;
+		flg[2]&=(1<<((side<<1)+islong))^0b11111111; 
 	}
 	inline void downKingMoved(int side) {
-		dat[side*2+20]&=0b0111;
+		flg[2]&=(1<<(side+4))^0b11111111;
+	}
+	inline void downWhiteTurn() {
+		flg[2]&=(1<<6)^0b11111111;
 	}
 };
 
 struct Window {
 	GtkApplication *app;
-	cairo_surface_t *surface;
-	int pieceSize = 128;
+	cairo_surface_t *surface=0;
+	int pieceSize = 64;
 	rgba_t oddColor{1.0, 0, 0.5, 1};
 	rgba_t evenColor{1.0, 0.5, 0.75, 1};
+	rgba_t custom[8][8]{};
 	cairo_surface_t *assets;
 	cairo_surface_t *flipAssets;
 	Board board;
@@ -136,29 +323,40 @@ struct Window {
 	void virtual motionNotify(GtkWidget *widget, GdkEventMotion *event);
 	void virtual buttonPress(GtkWidget *widget, GdkEventButton *event);
 	void virtual redraw();
+	int virtual getWidth() { return 8*pieceSize; }
+	int virtual getHeight() { return 8*pieceSize; }
 
 	Window() {
 		app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
 		g_signal_connect(app, "activate", G_CALLBACK (activate), this);
-		assets = cairo_image_surface_create_from_png("asset.png");
-		flipAssets = cairo_image_surface_create_from_png("asset.png");
-		flipAssets = cairo_image_surface_create_from_png("asset.png");
-		cairo_surface_t *tmp = cairo_image_surface_create_from_png("asset.png");
 
-		cairo_t *cr = cairo_create(tmp);
+		cairo_surface_t *tmp = cairo_image_surface_create_from_png("asset.png");
+		assets = cairo_surface_create_similar(tmp, CAIRO_CONTENT_COLOR_ALPHA, pieceSize*8, pieceSize);
+		cairo_t *cr = cairo_create(assets);
+		cairo_scale(cr, (float)pieceSize/128, (float)pieceSize/128);
+		cairo_set_source_surface(cr, tmp, 0, 0);
+		cairo_paint(cr);
+		cairo_destroy(cr);
+		cairo_surface_destroy(tmp);
+
+		tmp = cairo_surface_create_similar(assets, CAIRO_CONTENT_COLOR_ALPHA, pieceSize*8, pieceSize);
+		cr = cairo_create(tmp);
+		cairo_set_source_surface(cr, assets, 0, 0);
+		cairo_paint(cr);
 		cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
 		cairo_set_operator (cr, CAIRO_OPERATOR_DIFFERENCE);
 		cairo_paint(cr);
 		cairo_destroy(cr);
 
+		flipAssets = cairo_surface_create_similar(tmp, CAIRO_CONTENT_COLOR_ALPHA, pieceSize*8, pieceSize);
 		cr = cairo_create(flipAssets);
 		cairo_set_source_surface(cr, tmp, 0, 0);
 		cairo_mask_surface(cr, assets, 0, 0);
 		cairo_fill(cr);
-
 		cairo_destroy(cr);
 		cairo_surface_destroy(tmp);
-	}
+
+ }
 
 	int run(int argc, char **argv) {
 		int status;
@@ -170,6 +368,7 @@ struct Window {
 
 struct EditWindow : public Window {
 	int editId = 0;
+	int virtual getWidth() override { return 11*pieceSize; }
 	void virtual redraw() override {
 		Window::redraw();
 		cairo_t *cr = cairo_create(surface);
@@ -207,74 +406,150 @@ struct EditWindow : public Window {
 	void virtual buttonPress(GtkWidget *widget, GdkEventButton *event) {
 		int i = event->x/pieceSize;
 		int j = event->y/pieceSize;
-		if(i<8 && j<8 && i>=0 && j>=0) {
-			board.setPiece(i, j, editId);
-		}
-		if(i>=9 && i<11 && j>=0 && j<7 && !(i==10 && j==0)) {
-			editId = (i-9)*8+j;
-		}
-		if(i==8&&j==0) {
-			char *filename;
-			GtkWidget *dialog;
-			GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
-			gint res;
-
-			dialog = gtk_file_chooser_dialog_new("Open File",
-					NULL,
-					action,
-					"_Cancel",
-					GTK_RESPONSE_CANCEL,
-					"_Open",
-					GTK_RESPONSE_ACCEPT,
-					NULL);
-
-			gtk_file_chooser_set_current_folder((GtkFileChooser*)dialog, get_current_dir_name());
-			res = gtk_dialog_run (GTK_DIALOG(dialog));
-			if (res == GTK_RESPONSE_ACCEPT)
-			{
-				GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
-				filename=gtk_file_chooser_get_filename(chooser);
-				board.importBoard(filename);
-				g_free(filename);
+		if(event->button == 1) {
+			if(i<8 && j<8 && i>=0 && j>=0) {
+				board.setPiece(i, j, editId);
 			}
-
-			gtk_widget_destroy(dialog);
-			fflush(stdout);
-		}
-		if(i==8&&j==1) {
-         char *filename;
-			GtkWidget *dialog;
-			GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SAVE;
-			gint res;
-
-			dialog = gtk_file_chooser_dialog_new("Save File",
-					NULL,
-					action,
-					"_Cancel",
-					GTK_RESPONSE_CANCEL,
-					"_Save",
-					GTK_RESPONSE_ACCEPT,
-					NULL);
-
-			gtk_file_chooser_set_current_folder((GtkFileChooser*)dialog, get_current_dir_name());
-			res = gtk_dialog_run (GTK_DIALOG(dialog));
-			if (res == GTK_RESPONSE_ACCEPT)
-			{
-				GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
-				filename=gtk_file_chooser_get_filename(chooser);
-				board.exportBoard(filename);
-				g_free(filename);
+			if(i>=9 && i<11 && j>=0 && j<7 && !(i==10 && j==0)) {
+				editId = (i-9)*8+j;
 			}
+			if(i==8&&j==0) {
+				char *filename;
+				GtkWidget *dialog;
+				GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
+				gint res;
 
-			gtk_widget_destroy(dialog);
-			fflush(stdout);   
+				dialog = gtk_file_chooser_dialog_new("Open File",
+						NULL,
+						action,
+						"_Cancel",
+						GTK_RESPONSE_CANCEL,
+						"_Open",
+						GTK_RESPONSE_ACCEPT,
+						NULL);
+
+				gtk_file_chooser_set_current_folder((GtkFileChooser*)dialog, get_current_dir_name());
+				res = gtk_dialog_run (GTK_DIALOG(dialog));
+				if (res == GTK_RESPONSE_ACCEPT)
+				{
+					GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+					filename=gtk_file_chooser_get_filename(chooser);
+					board.importBoard(filename);
+					g_free(filename);
+				}
+
+				gtk_widget_destroy(dialog);
+				fflush(stdout);
+			}
+			if(i==8&&j==1) {
+				char *filename;
+				GtkWidget *dialog;
+				GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SAVE;
+				gint res;
+
+				dialog = gtk_file_chooser_dialog_new("Save File",
+						NULL,
+						action,
+						"_Cancel",
+						GTK_RESPONSE_CANCEL,
+						"_Save",
+						GTK_RESPONSE_ACCEPT,
+						NULL);
+
+				gtk_file_chooser_set_current_folder((GtkFileChooser*)dialog, get_current_dir_name());
+				res = gtk_dialog_run (GTK_DIALOG(dialog));
+				if (res == GTK_RESPONSE_ACCEPT)
+				{
+					GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+					filename=gtk_file_chooser_get_filename(chooser);
+					board.exportBoard(filename);
+					g_free(filename);
+				}
+
+				gtk_widget_destroy(dialog);
+				fflush(stdout);   
+			}
+		}
+		else if (event->button == 3) {
+			memset(custom, 0, sizeof(custom));
+			if(i<8 && j<8 && i>=0 && j>=0) {
+				unsigned char buf[32];
+				board.listMove(i, j, buf, 0);
+				for(int i = 0; i<32; ++i) {
+					int id1=buf[i];
+					custom[id1/8][id1%8].r=1;
+					custom[id1/8][id1%8].a=1;
+				}
+			}
 		}
 		redraw();
 		gtk_widget_queue_draw(widget);
 	}
 };
 
+//TODO
+struct BoardValidator {
+
+};
+
+struct GameWindow : public Window {
+   int selecting = 0;
+	int oldi=-1, oldj=-1;
+	unsigned char moveList[32];
+
+	GameWindow() {
+		memcpy(board.dat, startDat, sizeof(startDat));
+	}
+
+	void virtual buttonPress(GtkWidget *widget, GdkEventButton *event) {
+   	int i = event->x/pieceSize;
+		int j = event->y/pieceSize;
+
+		if(event->button == 1) {
+			if(selecting == 0) {
+				if(i>=0 && i<8 && j>=0 && j<8) {
+					if(board.getWhiteTurn() ^ ((board.getPiece(i, j)&0b1000)>>3)) {
+						custom[i][j].a=0.5;
+						custom[i][j].r=0.;
+						custom[i][j].g=0.;
+						custom[i][j].b=0.;
+						oldi=i; oldj=j;
+						selecting = 1;
+						board.listMove(i, j, moveList, 0);
+						for(int i = 0; i<32; ++i) {
+							int id1=moveList[i];
+							if(moveList[i]==0b11111111)continue;
+							custom[id1/8][id1%8].g=1;
+							custom[id1/8][id1%8].r=0.6;
+							custom[id1/8][id1%8].b=0.6;
+							custom[id1/8][id1%8].a=1;
+						}
+					}
+				}
+			}
+			else {
+				selecting = 0;
+				memset(custom, 0, sizeof(custom));
+				int it;
+				for(it=0; it<32;++it) {
+					if(moveList[it]==0b11111111)continue;
+					if(moveList[it] == ((i<<3)|j)) break;
+				}
+				if(it<32) board.move(oldi, oldj, i, j);
+			}
+		}
+		else if(event->button == 3) {
+			selecting = 0;
+			memset(custom, 0, sizeof(custom));
+		}
+		redraw();
+		gtk_widget_queue_draw(widget);
+	}
+};
+
+
 int main(int argc, char **argv) {
+	using namespace std;
 	srand(time(0));
 	Board b{};
 	//test 1
@@ -287,8 +562,8 @@ int main(int argc, char **argv) {
 			std::cout<<"ngu: "<<x<<' '<<y<<std::endl;
 	}
 
-	Window w = EditWindow();
-	w.board = b;
+	//test 2
+	Window w = GameWindow();
 	return w.run(argc, argv);
 }
 
@@ -296,7 +571,8 @@ void Window::activate(GtkApplication *app, gpointer data) {
 	GtkWidget *window;
 	GtkWidget *frame;
 	GtkWidget *drawingArea;
-	int widSize = 8*((Window*)data)->pieceSize + 2;
+	int widSize = ((Window*)data)->getWidth()+ 2;
+	int heiSize = ((Window*)data)->getHeight()+ 2;
 
 	window = gtk_application_window_new(app);
 	gtk_window_set_title(GTK_WINDOW(window), "Stupid Chess");
@@ -336,6 +612,7 @@ gboolean Window::draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
 
 gboolean Window::configureEvent(GtkWidget *widget, GdkEventConfigure *event, gpointer data) {
 	Window *w = (Window*)data;
+
 	if(w->surface)
 		cairo_surface_destroy(w->surface);
 	w->surface = gdk_window_create_similar_surface(gtk_widget_get_window(widget),
@@ -367,6 +644,8 @@ void Window::redraw() {
 				cairo_set_source_rgb(cr, oddColor.r, oddColor.g, oddColor.b);
 			else 
 				cairo_set_source_rgb(cr, evenColor.r, evenColor.g, evenColor.b);
+			if(custom[i][j].a!=0)
+				cairo_set_source_rgba(cr, custom[i][j].r, custom[i][j].g, custom[i][j].b, custom[i][j].a);
 			cairo_rectangle(cr, i*pieceSize, j*pieceSize, pieceSize, pieceSize);
 			cairo_fill(cr);
 			int id=piece&0b111;
